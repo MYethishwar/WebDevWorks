fed2c6a3993110683b846e1f4c83577d8d6715ea
Myethishwar <yathishwerchintu@gmail.com>
2025-06-12 19:09:09 +0530

    Modified Notes

--- Notes/script.js
+++ Notes/script.js
@@ -1,3 +1,25 @@
 console.log("Hey this is my first Java Script File")
-alert("hey this is yethishwars file")
-prompt("Hey  Please Enter your name")
+// let age  =  prompt("Please Enter Your Age??");
+// if(age > 65){
+//     console.log("You are a Senior citizen!!!")
+// }
+
+// else if(age > 35){
+//     console.log("You are Married!!!");
+// }
+
+// else if(age > 20){
+//     console.log("You are a Young Star!!!!");
+// }
+
+// else if(age > 10){
+//     console.log("You are a Kid!!!");
+// }
+
+// else{
+//     console.log("You Born recently how can you give input Here!!!");
+// }
+
+
+
+const pass = prompt();
